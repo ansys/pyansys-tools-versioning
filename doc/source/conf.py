7c4e09a04cb4437453adb70f1ec05f585fa86bf0
Roberto Pastor Muela <roberto.pastormuela@ansys.com>
2022-11-21 11:42:50 +0100

    DOC: missing config

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -37,6 +37,7 @@ html_theme_options = {
         "json_url": f"https://{cname}/release/versions.json",
         "version_match": get_version_match(__version__),
     },
+    "navbar_end": ["version-switcher", "theme-switcher", "navbar-icon-links"],
     "github_url": "https://github.com/pyansys/pyansys-tools-versioning",
     "show_prev_next": False,
     "show_breadcrumbs": True,
