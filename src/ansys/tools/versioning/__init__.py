951d16be548be21cdda7471835f768ca21aca047
Alex Fernandez Luces <alex.fernandezluces@ansys.com>
2025-11-03 10:21:26 +0100

    maint: Deprecate library

--- src/ansys/tools/versioning/__init__.py
+++ src/ansys/tools/versioning/__init__.py
@@ -26,8 +26,7 @@ PyAnsys Tools Versioning.
 Utilities for backwards and forwards server support.
 """
 import importlib.metadata as importlib_metadata
-
-from ansys.tools.versioning.utils import requires_version, server_meets_version
+from ansys.tools.common.versioning import requires_version, server_meets_version
 
 __version__ = importlib_metadata.version("pyansys-tools-versioning")
 __all__ = ["requires_version", "server_meets_version"]
